f57d6eedce9cd85c8ae54518491ca3974a0886d7
oleg fruntov <fruntovoleg@gmail.com>
2021-08-02 20:26:08 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        private static void Test70()
+        {
+            Console.WriteLine(2);
+        }
+
+        private static void Test69()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
