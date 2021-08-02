3f3e7ee7e430b05483621148f1301f97f8f74b5c
oleg fruntov <fruntovoleg@gmail.com>
2021-08-02 20:26:52 +0300

    ДЗ

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -7,6 +7,7 @@ namespace Lesson2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
         }
 
         private static void Test70()
